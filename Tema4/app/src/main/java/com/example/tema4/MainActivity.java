4946156c494d3ab9225d624bce36825339953989
unknown <gonz.g.lop@gmail.com>
2022-10-25 09:16:24 +0200

    android ejemplos

--- Tema4/app/src/main/java/com/example/tema4/MainActivity.java
+++ Tema4/app/src/main/java/com/example/tema4/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.tema4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout1);
+
+        if(savedInstanceState!=null){
+            //Recuperamos una variable que almacena con el estado
+            Toast.makeText(this, "Evento onCreate(). Se recupera variable almacenada: "
+                    + savedInstanceState.getString("variable_almacenada"), Toast.LENGTH_SHORT).show();
+        }
     }
+
+    //guardamos el estado de la actividad
+    @Override
+    protected void onSaveInstanceState(Bundle estado){
+        super.onSaveInstanceState(estado);
+        //añadimos una variable que se almacena con el estado
+        estado.putString("variable_almacenada","Texto guardado");
+
+        Toast.makeText(this, "El estado de la actividad se ha guardado", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this, "El sistema ha terminado la actividad", Toast.LENGTH_SHORT).show();
+    }
+    //Dev-tools --> Opciones de desarrollo
+
 }
