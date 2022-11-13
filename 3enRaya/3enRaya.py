6e00c8d7ae4a730613fe7901ad150277df00fee7
Gonzalo López <gonz.g.lop@gmail.com>
2022-11-13 22:56:54 +0100

    3enraya

--- 3enRaya/3enRaya.py
+++ 3enRaya/3enRaya.py
@@ -42,17 +42,16 @@ def EnterMove(tablero):
                 tablero[posicion] = 'O'
                 DisplayBoard(tablero)
 
-                contO =+ 1
+                contO += 1
                 if contO >= 3:
                     # metodoVictoria
-                    sign=VictoryFor(tablero)
+                    VictoryFor(tablero)
                     if contO > 4 and not sign:
                         print("Habéis quedado en tablas.")
                         sign = True
-
                 bol = True
             else:
-                print('la posición no es válida')
+                print('La posición no es válida')
         except ValueError:
             print('Número no válido.')
 
@@ -61,88 +60,61 @@ def DrawMove(tablero):
     # La función dibuja el movimiento de la máquina y actualiza el tablero.
     global sign
     global contX
-    print('Turno de la máquina.')
-    numAleatorio = 5
-    while tablero[numAleatorio] == 'X' or tablero[numAleatorio] == 'O':
-        numAleatorio = random.randint(1, 9)
-    tablero[numAleatorio] = 'X'
+    if not sign:
+        print('Turno de la máquina.')
+        numAleatorio = 5
+        while tablero[numAleatorio] == 'X' or tablero[numAleatorio] == 'O':
+            numAleatorio = random.randint(1, 9)
+        tablero[numAleatorio] = 'X'
 
-    DisplayBoard(tablero)
+        DisplayBoard(tablero)
 
-    contX = contX + 1
-    if contX >= 3:
-        # metodoVictoria
-        VictoryFor(tablero)
-        if contX > 4 and not sign:
-            print("Habéis quedado en tablas.")
-            sign = True
+        contX = contX + 1
+        if contX >= 3:
+            # metodoVictoria
+            VictoryFor(tablero)
+            if contX > 4 and not sign:
+                print("Habéis quedado en tablas.")
+                sign = True
 
 
 def VictoryFor(tablero):
-    global sign
+    global sign, z, y, x
+    a = [
+        [tablero[1], tablero[2], tablero[3]],
+        [tablero[4], tablero[5], tablero[6]],
+        [tablero[7], tablero[8], tablero[9]],
+        [tablero[1], tablero[4], tablero[7]],
+        [tablero[2], tablero[5], tablero[8]],
+        [tablero[3], tablero[6], tablero[9]],
+        [tablero[1], tablero[5], tablero[9]],
+        [tablero[3], tablero[5], tablero[7]]
+    ]
 
-    x = tablero[1]
-    y = tablero[2]
-    z = tablero[3]
-    if x == y and y == z:
-        print('El ganador es ', x)
-        sign = True
+    for row in a:
+        cont = 0
 
-    x = tablero[4]
-    y = tablero[5]
-    z = tablero[6]
-    if x == y and y == z:
-        print('El ganador es ', x)
-        sign = True
+        for elem in row:
 
-    x = tablero[7]
-    y = tablero[8]
-    z = tablero[9]
-    if x == y and y == z:
-        print('El ganador es ', x)
-        sign = True
+            if cont == 0:
+                x = elem
+                cont += 1
+            elif cont == 1:
+                y = elem
+                cont += 1
+            elif cont == 2:
+                z = elem
+                cont += 1
 
-    x = tablero[1]
-    y = tablero[4]
-    z = tablero[7]
-    if x == y and y == z:
-        print('El ganador es ', x)
-        sign = True
-
-    x = tablero[2]
-    y = tablero[5]
-    z = tablero[8]
-    if x == y and y == z:
-        print('El ganador es ', x)
-        sign = True
-
-    x = tablero[3]
-    y = tablero[6]
-    z = tablero[9]
-    if x == y and y == z:
-        print('El ganador es ', x)
-        sign = True
-
-    x = tablero[1]
-    y = tablero[5]
-    z = tablero[9]
-    if x == y and y == z:
-        print('El ganador es ', x)
-        sign = True
-
-    x = tablero[3]
-    y = tablero[5]
-    z = tablero[7]
-    if x == y and y == z:
-        print('El ganador es ', x)
-        sign = True
-
+        if x == y and y == z:
+            print('El ganador es ', x)
+            sign = True
 
 
 # La función analiza el estatus del tablero para verificar si
 # el jugador que utiliza las 'O's o las 'X's ha ganado el juego.
 
-
+# -----------------------------------------------------------------
 print('Empieza la máquina.')
 
 DisplayBoard(tablero)
