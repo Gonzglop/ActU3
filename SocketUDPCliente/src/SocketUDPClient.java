9a5aed061d2d65d435aaaacbd74dbb396f0e588e
unknown <gonz.g.lop@gmail.com>
2022-12-07 09:25:10 +0100

    examen UDP

--- SocketUDPCliente/src/SocketUDPClient.java
+++ SocketUDPCliente/src/SocketUDPClient.java
@@ -1,46 +1,64 @@
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
 import java.net.*;
 
 public class SocketUDPClient {
 
     public static void main(String[] args) {
+
         final int PUERTO_SERVIDOR = 49171;
+        byte[] buffer = new byte[256];
+        InetAddress hostServidor;
         DatagramSocket socketUDP;
         DatagramPacket mensaje;
+        DatagramPacket mensajeServidor;
         ByteArrayOutputStream baos;
         DataOutputStream dos;
+        ByteArrayInputStream bais;
+        DataInputStream dis;
+        String clave;
+        String strMensajeServidor;
+        int timeout;
+
 
         try {
             System.out.println("Iniciado el cliente UDP");
             socketUDP = new DatagramSocket();
+            timeout = 5000;
+            socketUDP.setSoTimeout(timeout);
 
             System.out.println("Estableciendo parámetros de conexión...");
-            InetAddress hostServidor = InetAddress.getByName("localhost");
+            hostServidor = InetAddress.getByName("localhost");
 
             System.out.println("Enviando información....");
-            for (int i = 0; i < 10000; i++) {
-                baos = new ByteArrayOutputStream();
-                dos = new DataOutputStream(baos);
-
-                dos.writeUTF("Mensaje: " + i);
-                mensaje = new DatagramPacket(baos.toByteArray(), baos.size(), hostServidor, PUERTO_SERVIDOR);
-                socketUDP.send(mensaje);
-                dos.close();
-
-            }
             baos = new ByteArrayOutputStream();
             dos = new DataOutputStream(baos);
 
-            dos.writeUTF("FIN");
+            System.out.println("Pidiendo palabra clave al usuario....");
+            clave = JOptionPane.showInputDialog("Introduce la palabra clave:");
+
+            dos.writeUTF(clave);
             mensaje = new DatagramPacket(baos.toByteArray(), baos.size(), hostServidor, PUERTO_SERVIDOR);
+
+            System.out.println("Enviando palabra clave.");
             socketUDP.send(mensaje);
             dos.close();
+
+            mensajeServidor = new DatagramPacket(buffer, buffer.length);
+            System.out.println("Recibiendo mensaje del Servidor.");
+            socketUDP.receive(mensajeServidor);
+            System.out.println("Recibido mensaje del Servidor.");
+
+            bais = new ByteArrayInputStream(mensajeServidor.getData());
+            dis = new DataInputStream(bais);
+            strMensajeServidor = dis.readUTF();
+            System.out.println("Texto recibido: " + strMensajeServidor);
 
             socketUDP.close();
             System.out.println("Cerrando el cliente UDP");
 
+        } catch (SocketTimeoutException se) {
+            System.err.println("Se ha excedido el tiempo de espera.");
         } catch (IOException e) {
             e.printStackTrace();
         }
--- SocketUDPServidor/src/SocketUDPServer.java
+++ SocketUDPServidor/src/SocketUDPServer.java
@@ -1,23 +1,25 @@
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class SocketUDPServer {
 
     public static void main(String[] args) {
         final int PUERTO = 49171;
-        byte[] buffer = new byte[64];
+        byte[] buffer = new byte[256];
         DatagramSocket socket;
         DatagramPacket mensajeCliente;
+        DatagramPacket mensajeFecha;
         ByteArrayInputStream bais;
         DataInputStream dis;
-        BufferedWriter fichero;
-        String strMensajeCliente = "";
+        String strMensajeCliente;
+        ByteArrayOutputStream baos;
+        DataOutputStream dos;
 
         try {
             System.out.println("Iniciado el servidor UDP");
@@ -26,9 +28,8 @@ public class SocketUDPServer {
             System.out.println("Esperando la información del cliente...");
             mensajeCliente = new DatagramPacket(buffer, buffer.length);
 
-            fichero = new BufferedWriter(new FileWriter("FicheroTexto.txt"));
+            while (true) {
 
-            while (!strMensajeCliente.equals("FIN")){
                 socket.receive(mensajeCliente);
                 bais = new ByteArrayInputStream(mensajeCliente.getData());
                 dis = new DataInputStream(bais);
@@ -36,18 +37,24 @@ public class SocketUDPServer {
 
                 System.out.println("Texto recibido: " + strMensajeCliente);
 
-                if (!strMensajeCliente.equals("FIN")){
-                    fichero.write(strMensajeCliente);
-                    fichero.newLine();
+                if (strMensajeCliente.equals("CLAVE")) {
+
+                    Date date = Date.from(Instant.now());
+                    System.out.println("Fecha actual:");
+                    System.out.println(date);
+
+                    baos = new ByteArrayOutputStream();
+                    dos = new DataOutputStream(baos);
+                    dos.writeUTF(date.toString());
+                    mensajeFecha = new DatagramPacket(baos.toByteArray(), baos.size(), mensajeCliente.getAddress(), mensajeCliente.getPort());
+                    socket.send(mensajeFecha);
+                    dos.close();
+                }else {
+                    System.out.println("Texto clave incorrecto, no se devolverá la fecha al cliente.");
                 }
             }
-            fichero.close();
-
-            socket.close();
-            System.out.println("Cerrando el servidor UDP");
-
-        } catch (FileNotFoundException fn ){
-            System.out.println("No se encuentra el fichero");
+            //socket.close();
+            //System.out.println("Cerrando el servidor UDP");
         } catch (IOException io) {
             System.out.println("Error de E/S ");
         }
